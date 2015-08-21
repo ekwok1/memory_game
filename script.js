4fdbf2dd3cbaf764ddaaa39d4ef239364f91642e
Eugene Kwok <eugenekwok11@gmail.com>
2015-08-21 15:49:13 -0700

    reveal cards implemented with JS

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 window.onload = function() {
-
+	revealCard();
 };
 
+function revealCard(){
+	var card = document.querySelectorAll(".card");
+
+	_.each(card, function(element) {
+		element.addEventListener("click", function(){
+			this.classList.remove("facedown");
+		});
+	});
+}
